1ccb5a752e0f48aa0acc762c33cc8522120fe671
Yuri Lendro <y.lendrof@gmail.com>
2025-09-06 18:38:05 -0100

    Enhance layout and styling with new header, sidebar, and article components; integrate Tailwind CSS utilities for improved design consistency.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,25 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			width: {
+				'30': '7.5rem', // 120px
+			},
+			height: {
+				'20': '5rem', // 80px
+			}
+		},
 	},
-	plugins: [],
+	plugins: [
+		function({ addUtilities }) {
+			addUtilities({
+				'.line-clamp-3': {
+					'display': '-webkit-box',
+					'-webkit-line-clamp': '3',
+					'-webkit-box-orient': 'vertical',
+					'overflow': 'hidden',
+				},
+			})
+		}
+	],
 }

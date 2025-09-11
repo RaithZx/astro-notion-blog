8f8bed77a25afcd023a5669d50ece4aac2dcd1c4
Yuri Leandro <y.leandrof@gmail.com>
2025-09-11 17:10:30 -0100

    Add Ligadu logo and update layout for improved branding. Set default title and description for better SEO. Update favicon links to use the new logo.

--- src/pages/feed.ts
+++ src/pages/feed.ts
@@ -1,13 +1,14 @@
 import rss from '@astrojs/rss'
 import { getAllPosts, getDatabase } from '../lib/notion/client'
 import { getPostLink } from '../lib/blog-helpers'
-
+const title = 'Ligadu'
+const description = 'Artigus di siensia i saudi'
 export async function GET() {
   const [posts, database] = await Promise.all([getAllPosts(), getDatabase()])
 
   return rss({
-    title: database.Title,
-    description: database.Description,
+    title: title,
+    description: description,
     site: import.meta.env.SITE,
     items: posts.map((post) => ({
       link: new URL(getPostLink(post.Slug), import.meta.env.SITE).toString(),
